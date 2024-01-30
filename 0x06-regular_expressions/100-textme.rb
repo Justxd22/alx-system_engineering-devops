0742c4c9f215512e140099d581503b94d8217129
Justxd22 <xdjust18@gmail.com>
2024-01-31 01:48:17 +0200

    fix

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,3 @@
 #!/usr/bin/env ruby
 
-sender = ARGV[0].scan(/\[from:([^\[\]]*)\]/).flatten.first
-receiver = ARGV[0].scan(/\[to:([^\[\]]*)\]/).flatten.first
-flags = ARGV[0].scan(/\[flags:([^\[\]]*)\]/).flatten.first
-
-if sender && receiver && flags
-  puts "#{sender}, #{receiver}, #{flags}"
-end
+puts ARGV[0].scan(/(?<=from:)\S*(?<!\])|(?<=to:)\S*(?<!\])|(?<=flags:)\S*(?<!\])/).join(',')

facb8972f57f8b47ea485f01a2166792ff53a036
Justxd22 <xdjust18@gmail.com>
2024-03-22 23:33:19 +0200

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # install flask ver 2.1.0
-exec { 'install_flask_ver_2.1.0':
-    command => 'pip3 install flask==2.1.0',
-    path    => ['/usr/bin', '/usr/local/bin'],
-    unless  => 'pip3 show requests | grep "Version: 2.1.0"',
+Package { 'flask':
+  name   => 'Flask',
+  provider => 'pip3',
+  ensure => '2.2.0',
 }

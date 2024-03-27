2657a4e951869487a59941496334f401a1cab076
Justxd22 <xdjust18@gmail.com>
2024-03-27 18:09:22 +0200

    fixx

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,15 @@
 # cofigure ssh client using ppupet
+include stdlib
 file_line { 'Turn off Password Auth':
+    ensure  => present,
     path    => '/etc/ssh/ssh_config',
-    line    => 'PasswordAuthentication no',
+    line    => '    PasswordAuthentication no',
+    replace => true,
 }
 
 file_line { 'Use school rsa':
+    ensure  => present,
     path    => '/etc/ssh/ssh_config',
-    line    => 'IdentityFile ~/.ssh/school',
+    line    => '    IdentityFile ~/.ssh/school',
+    replace => true,
 }

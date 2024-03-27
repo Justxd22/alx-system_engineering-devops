b6daede070626a6e46150ce3d44241e46548d1b1
Justxd22 <xdjust18@gmail.com>
2024-03-27 04:52:19 +0200

    ffff

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,31 +8,50 @@ class nginx {
     ensure => running,
     enable => true,
   }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx/default.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-}
-
-class { 'nginx': }
-
 file { '/var/www/html/index.html':
   ensure  => file,
   content => 'Hello World!',
+  require => Package['nginx'],
 }
 
 file { '/var/www/html/404.html':
   ensure  => file,
   content => 'Ceci n\'est pas une page',
+  require => Package['nginx'],
 }
 
-exec { 'configure_redirect':
-  command  => "sed -i '/^server {$/a \ location /redirect_me {return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;}' /etc/nginx/sites-available/default",
-  path     => '/usr/bin:/bin',
-  provider => 'shell',
-  unless   => "grep -q 'location /redirect_me' /etc/nginx/sites-available/default",
-  require  => File['/etc/nginx/sites-available/default'],
+file { '/etc/nginx/sites-available/redirect_me':
+    ensure  => file,
+    content => "
+      server {
+        listen 80;
+        server_name your_domain.com; # Replace with your domain name
+
+        location /redirect_me {
+          rewrite ^/redirect_me(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4$1 permanent; # Replace example.com with your desired redirect URL
+        }
+
+        error_page 404 /404.html;
+        location = /404.html {
+          root /var/www/html;
+          internal;
+        }
+
+        root /var/www/html;
+        index index.html;
+      }
+    ",
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-enabled/redirect_me':
+    ensure  => link,
+    target  => '/etc/nginx/sites-available/redirect_me',
+    require => File['/etc/nginx/sites-available/redirect_me'],
+    notify  => Service['nginx'],
+  }
+
 }
+
+class { 'nginx': }
+

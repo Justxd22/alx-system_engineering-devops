ef8558891e2b5f0ae47c97617e140f5bb7f037a2
Justxd22 <xdjust18@gmail.com>
2024-05-08 02:33:36 +0300

    i

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,20 +6,14 @@ import sys
 
 def number_of_subscribers(subreddit):
     """Use the Reddit api."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     h = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
          AppleWebKit/537.36 (KHTML, like Gecko) \
          Chrome/97.0.4692.99 Safari/537.36"}
-    res = get(url, headers=h, allow_redirects=False)
-
-    if res.status_code == 200:
-        try:
-            users = res.json()
-            users = users["data"]["subscribers"]
-            return users
-        except Exception as e:
-            return 0
-    else:
+    count = get('https://www.reddit.com/r/{}/about.json'.format(
+        subreddit), headers=h).json()
+    try:
+        return count.get('data').get('subscribers')
+    except:
         return 0
 
 if __name__ == "__main__":
